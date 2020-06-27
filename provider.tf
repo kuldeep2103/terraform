fe72ec150f329fbfa2fec38a3359177c4920fbab
root <root@ip-172-31-50-142.ec2.internal>
2020-06-27 09:31:49 +0000

    test

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAIILPPRXIR3A7QITA"
-  secret_key = "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"
+  access_key = "thest"
+  secret_key = "test"
 }
